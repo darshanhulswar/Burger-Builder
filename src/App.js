5ac37435cf3f4dcd43a79089a92d29fbca619984
Darshan <dhulswar591@gmail.com>
2020-11-20 20:49:42 +0530

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
-import 'tailwindcss/dist/tailwind.css'
-import authContext from './context/context';
-import Profile from './profile';
-import StyleComponent from './hoc/StyleComponent';
 
 class App extends Component {
 
-  static contextType = authContext;
-
   render() {
     return (
-      <React.Fragment>
-        <h1 className="">{this.context.email}</h1>
-        <button>Hello</button>
-        <Profile />
-      </React.Fragment>
+      <div></div>
     )
   }
 }
 
-export default StyleComponent(App, "text-center text-red-500 font-bold");
+export default App;
